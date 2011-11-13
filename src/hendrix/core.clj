f99fffdaf7eff0bb0cf1840c163e1c3c40874741
Julian Birch <julian.birch@colourcoding.net>
2011-11-13 12:53:34 +0000

    Initial commit

---
+++ src/hendrix/core.clj
@@ -0,0 +1,1 @@
+(ns hendrix.core)
---
+++ test/hendrix/test/core.clj
@@ -0,0 +1,6 @@
+(ns hendrix.test.core
+  (:use [hendrix.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
