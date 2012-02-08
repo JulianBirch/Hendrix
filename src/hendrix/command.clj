721b950f6e36cd61f4e989fff24ba855e62196eb
Julian Birch <julian.birch@colourcoding.net>
2012-02-08 19:27:41 +0000

    Actually compiles bootstrap.

--- src/hendrix/command.clj
+++ src/hendrix/command.clj
@@ -1,7 +1,9 @@
 (ns hendrix.command
   (:use
    [clojure.java.shell :only [sh]]
-   [hendrix.file :only [get-canonical-path]]))
+   [hendrix.file :only [get-canonical-path]])
+  (:import [java.util.regex Pattern]
+           [java.io File]))
 
 (defn tap [f result] (doto result f))
 
@@ -9,8 +11,44 @@
 
 (defn log-error [text] (println text))
 
-(defn command [args]
-  (let [{:keys [out err exit]} (sh args)]
+(defn file-exists [^String path ^String file]
+  (.exists (File. path file)))
+
+(defn is-windows []
+  (= ";" File/pathSeparator))
+
+(defn which
+  ([command] (which command
+                    (System/getenv "path")
+                    File/pathSeparator ))
+  ([command path separator]
+     (let [items (.split path (Pattern/quote separator))]
+       (println separator)
+       (println items)
+       (->> items
+            (filter #(file-exists % command))
+            first))))
+
+(defn correct-file-name [command]
+  (if (is-windows)
+    (->> [".bat" ".exe" ""]
+         (map #(str command %))
+         (filter which)
+         first)
+    command))
+
+(defn command [[c & args]]
+  (let [
+        _ (println "Wiki")
+        _ (println File/pathSeparator)
+        _ (println (is-windows))
+        _ (println (correct-file-name c))
+        _ (println args)
+        result (apply sh (correct-file-name c) args)
+        _ (println "Hello")
+        _ (println result)
+        _ (println "World")
+        {:keys [out err exit]} result]
     (log-output "OUTPUT")
     (log-output out)
     (log-error "ERROR")
